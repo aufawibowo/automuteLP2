bf781e9c5da47c0f977241555ad79cab5e44117e
Unknown <muh.aufa@gmail.com>
2017-11-01 11:54:02 +0700

    hehhwhe

---
+++ coba.py
@@ -0,0 +1,10 @@
+import os
+
+
+mute='nircmd.exe mutesysvolume 1'
+unmute='nircmd.exe mutesysvolume 0'
+
+
+
+os.system('"C:/Windows/System32/notepad.exe"')
+os.system('"D:/automute/nircmd.exe"')
---
+++ zz.py
@@ -0,0 +1,14 @@
+import json
+import os
+import time
+import requests
+
+url='10.151.62.3:8081/feeder/Laboratorium+Pemrograman+2'
+
+wjdata = requests.get('url').json()
+
+
+print wjdata[jumlah]
+raw_input()
+os.system('"C:/Windows/System32/notepad.exe"')
+time.sleep(5)
